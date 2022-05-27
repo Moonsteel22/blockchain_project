d3dfb443e10aa50b953313ebe07cd135e3bf257f
aibulat <aibulat.ryic@gmail.com>
2022-05-27 17:25:40 +0300

    Why are you gay?

--- test_contract.sol
+++ test_contract.sol
@@ -3,53 +3,67 @@ pragma solidity ^0.8.14;
 contract Test{
     address public owner;
 
-    struct AucUser{
-        address user;
-        uint32 value;
+    struct Auction{
+        string auc_name;
+        uint min_value;
     }
 
     struct ActiveAuc {
       string auc_name;
-      AucUser[] user;
-      uint32 max_value;
+      address max_user;
+      uint max_value;
    }
 
-    mapping(string=> uint32) public auctions;
+    mapping(string=> Auction) public auctions;
 
     mapping(string => ActiveAuc) public active_aucs;
 
-    mapping(string => ) public auc_users;
-
-    mapping(address => string[]) public users;
-
-    mapping(address=> uint) public payments;
+    mapping(address => Auction[]) public users;
 
     constructor(){
         owner = msg.sender;
     }
-    function get_auc_info(string auction) public returns(ActiveAuc){
+
+    function get_auc_info(string memory auction) public returns(ActiveAuc memory){
         return active_aucs[auction];
     }
 
-    function get_aucs(address user) public returns(string[]){
+    function get_aucs(address user) public view returns(Auction[] memory){
         return users[user];
     }
 
-    function participate_auction(string auc_name) public payable {
+    function participate_auction(string memory auc_name) public payable{
+        users[msg.sender].push(auctions[auc_name]);
+        if (active_aucs[auc_name].max_value>0){
+            if (active_aucs[auc_name].max_value > msg.value)
+                active_aucs[auc_name].max_value = msg.value;
+                active_aucs[auc_name].max_user = address(msg.sender);
+        }
+        else{
+            active_aucs[auc_name] = ActiveAuc(auc_name, msg.sender, msg.value);
+        }
         
-        users[msg.sender].push(auc_name);
     }
 
-    function get_eths() public payable {
-        payments[msg.sender] = msg.value;
-    }
-    function create_auction(string name, uint32 min_cost){
-        auctions[name] = min_cost;
+    function auc_lider(string memory auc_name) public returns(address){
+        if(active_aucs[auc_name].max_value>0){
+            return active_aucs[auc_name].max_user;
+        }
     }
 
-    function withdrawAll() public{
-        address payable _to = payable(owner);
-        address _this_contract = address(this);
-        _to.transfer(_this_contract.balance);
+    function end_auction(string memory name) public{
+        if(msg.sender == owner){
+            address payable _to = payable(owner);
+            if(active_aucs[name].max_value>0){
+                _to.transfer(active_aucs[name].max_value);
+            }
+        }
+
     }
+
+    function create_auction(string memory name, uint min_val) public{
+        Auction memory _buf = Auction(name,min_val);
+        auctions[name] = _buf;
+    }
+
 }
